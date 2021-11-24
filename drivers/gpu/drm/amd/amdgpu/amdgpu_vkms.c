b98e458f98e75243e43698e230456d2dc2a9078c
Flora Cui <flora.cui@amd.com>
2021-11-24 15:53:17 +0800

    SWDEV-316761 - Porting from mainline-5.13
    drm/amdgpu: replace the critial WARN_ON in amdgpu_vkms
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>
    Signed-off-by: Yifan Zha <Yifan.Zha@amd.com>
    Change-Id: Iec0f17904cf5b478380224de407d9b4495713b9f

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vkms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vkms.c
@@ -50,7 +50,8 @@ static enum hrtimer_restart amdgpu_vkms_vblank_simulate(struct hrtimer *timer)
 
 	ret_overrun = hrtimer_forward_now(&output->vblank_hrtimer,
 					  output->period_ns);
-	WARN_ON(ret_overrun != 1);
+	if (ret_overrun != 1)
+		DRM_WARN("%s: vblank timer overrun\n", __func__);
 
 	ret = drm_crtc_handle_vblank(crtc);
 	if (!ret)
